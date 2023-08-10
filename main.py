5b60e62aaf6a157ff2eb402444ddf62dc54564a4
Juhana Kangaspunta <juhana.kangaspunta@gmail.com>
2023-08-09 21:07:02 -0700

    Interstitials, better prompt.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 from playsound import playsound
 from tempfile import NamedTemporaryFile
 from gtts import gTTS
-from io import BytesIO
+import threading
 
 import openai
 from dotenv import load_dotenv
@@ -12,7 +12,15 @@ load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-INTERMEDIATE_AUDIO_FILE = "/tmp/intermediate.mp3"
+INTERSTITIAL_TEXTS = [
+    "Intriguing. You may prove a challenge for me yet.",
+    "Interesting. Let me think.",
+    "I am starting to see the patterns forming.",
+    "Curious. Very curious.",
+]
+
+SYSTEM_PROMPT = "You are the wise and ancient Sorting Hat at Hogwarts from the Harry Potter movies. Keep all your questions to a single sentence but your verdict can be longer and more thoughtful. You are conversational, and you speak naturally not in robotic, or verbose way."
+INITIAL_TEXT = "You are the Sorting Hat at Hogwarts from Harry Potter. You should ask me three questions one by one and sort me into a house. Please keep everything to one sentence, and don't use the typical questions asked in such quizes, instead disguise the intent of the question with creative questions, riddles and introspective puzzles."
 
 
 def get_completion(prompt, state=[]):
@@ -53,13 +61,18 @@ def get_audio():
     return said
 
 
-def save_intermediate_audio():
-    tts = gTTS("Hmmm.... Very well.", lang="en", tld="co.uk")
-    tts.save(INTERMEDIATE_AUDIO_FILE)
+def save_interstitials():
+    filenames = []
+    for i, text in enumerate(INTERSTITIAL_TEXTS):
+        tts = gTTS(text, lang="en", tld="co.uk")
+        filename = f"/tmp/interstitial_{i}.mp3"
+        tts.save(filename)
+        filenames.append(filename)
+    return filenames
 
 
-def play_intermediate_audio():
-    playsound(INTERMEDIATE_AUDIO_FILE, block=False)
+def play_file(filename):
+    playsound(filename)
 
 
 def speak(text):
@@ -69,16 +82,17 @@ def speak(text):
 
 
 def main():
-    save_intermediate_audio()
-    system_prompt = "You are the Sorting Hat at Hogwarts from Harry Potter."
-    initial_text = "You are the Sorting Hat at Hogwarts from Harry Potter. You should ask me two questions one by one and sort me into a house. You should first introduce yourself and ask me my name."
-    state = [{"role": "system", "content": system_prompt}]
-    output, state = get_output(initial_text, state)
-    while True:
+    interstitials = save_interstitials()
+    state = [{"role": "system", "content": SYSTEM_PROMPT}]
+    output, state = get_output(INITIAL_TEXT, state)
+    for i in range(100):
         speak(output)
         user_input = get_audio()
-        play_intermediate_audio()
+        interstitial = interstitials[i if i < len(interstitials) else len(interstitials) - 1]
+        interstitial_thread = threading.Thread(target=play_file, args=(interstitial,))
+        interstitial_thread.start()
         output, state = get_output(user_input, state)
+        interstitial_thread.join()
 
 
 main()
